b0732c432f4a6af8596b65b6e76800f4c18fc1b4
hsparks-codes <autonome0802@gmail.com>
2021-09-28 23:15:07 +0000

    automatically initialize virtual environment when stated an integrated terminal session

---
+++ integrated-terminal-init.sh
@@ -0,0 +1,6 @@
+# This script acts as a .bashrc-like mechanism.
+# Any commands placed here will be automatically run whenever
+# a new VSCode integrated terminal is started.
+
+# Initialize the python3 virtual environment
+source ./bin/activate
