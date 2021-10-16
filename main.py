5a9b588f013fcfc7c2ce6119d8eb1272d91a14a3
hsparks-codes <autonome0802@gmail.com>
2021-10-16 16:47:30 -0400

    initial ball position as function of  screen_size

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import sys, pygame
+
 pygame.init()
 
-size = width, height = 576, 1024
-clock=pygame.time.Clock() #g
+# Screen Dimensions
+screen_width = 576
+screen_height = 1024
+screen_size = (screen_width, screen_height)
 
 speed = [2, 2]
 black = 0, 0, 0
 
+clock=pygame.time.Clock() #g
+
 gravity = .20
 ballposition = 0
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(screen_size)
 
 #ball = pygame.image.load("intro_ball.gif")
 tempBackgrd= pygame.image.load("WestcottIMG.xcf")
 
 ball = pygame.image.load("logocropped2.png") #ball=logo 300by306 pixels
-ballrect = ball.get_rect(center = (288, 512))   # places ball in middle of screen...based on current size (576, 1024)
+ballrect = ball.get_rect(center = (screen_width / 2, screen_height / 2))   # places ball in middle of screen...based on current size (576, 1024)
 
 # ground = pygame.
 
@@ -26,12 +31,6 @@ while 1:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE: # when space bar is pressed down
                 ballposition = -10
-
-#     ballrect = ballrect.move(speed)
-#     if ballrect.left < 0 or ballrect.right > width:
-#         speed[0] = -speed[0]
-#     if ballrect.top < 0 or ballrect.bottom > height:
-#         speed[1] = -speed[1]
 
     #screen.fill(black)
     screen.blit(tempBackgrd, (0, 0)) #set temp background
