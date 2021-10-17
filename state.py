e9152baca11c5fb5713c845d457c181ae47368a9
hsparks-codes <autonome0802@gmail.com>
2021-10-17 01:10:18 -0400

    shift class header down

--- state.py
+++ state.py
@@ -1,7 +1,7 @@
-# Represents the state of the game during a single tick.
-# For every tick that passes the state object is progressed by tick(game_state) in tick.py.
 from constants import SIDESCROLL_SPEED
 
+# Represents the state of the game during a single tick.
+# For every tick that passes the state object is progressed by tick(game_state) in tick.py.
 class FlappyNoleGameState:
     def __init__(self):
         self.screen_width = 576
