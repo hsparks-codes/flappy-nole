95f9216b810775b6dd79c064ecd9a2d2714e0379
hsparks-codes <autonome0802@gmail.com>
2021-10-16 21:45:11 -0400

    add game over screen

--- draw.py
+++ draw.py
@@ -2,10 +2,17 @@ import pygame
 
 from state import FlappyNoleGameState
 
+pygame.font.init()
+
 background_img = pygame.image.load("assets/background.xcf")
 
 character_img = pygame.image.load("assets/character.png") # 300x306
-character_rect = character_img.get_rect() 
+
+# Styles
+title_font = pygame.font.Font("assets/noles_glades_bold.ttf", 80)
+fsu_garnet = (120, 47, 64)
+fsu_gold = (206, 184, 136)
+fsu_black = (44, 42, 41)
 
 # Simply draws the given game state on to the given screen.
 # Makes no changes to the given game state. 
@@ -14,8 +21,29 @@ def draw(screen, game_state: FlappyNoleGameState):
     screen.blit(background_img, (0, 0))
 
     # Draw Character
+    character_rect = character_img.get_rect() 
     character_rect.centerx = game_state.screen_width / 2
     character_rect.centery = game_state.character_vpos; 
     screen.blit(character_img, character_rect)
 
+    if game_state.is_game_over:
+        draw_game_over(screen, game_state)
+
     pygame.display.update()
+
+def draw_game_over(screen, game_state: FlappyNoleGameState):
+    alert = title_font.render("Game Over", True, fsu_gold, fsu_black)
+    screen.blit(alert, centered(game_state.screen_size, alert))   
+
+def horizontally_centered(screen_width, surface):
+    return (screen_width - surface.get_width()) / 2
+
+def vertically_centered(screen_height, surface):
+    return (screen_height - surface.get_height()) / 2    
+    
+def centered(screen_size, surface):
+    x: int = horizontally_centered(screen_size[0], surface)
+    y: int = vertically_centered(screen_size[1], surface)
+
+    return (x, y)
+        
--- state.py
+++ state.py
@@ -3,13 +3,23 @@
 class FlappyNoleGameState:
     def __init__(self):
         self.screen_width = 576
-        self.screen_height = 1024
-        self.character_vpos = self.screen_height / 2
-        # Speed at which the character is currently moving downwards, measured in pixels per tick.
-        self.character_downward_speed = 0
-        self.is_game_running = True
+        self.screen_height = 780
+        self.is_app_running = True
+        self.new_game()
 
     def character_jump(self):
-        self.character_downward_speed = -10    
+        self.character_downward_speed = -10      
 
-        
+    def new_game(self):
+        # Speed at which the character is currently moving downwards, measured in pixels per tick.
+        self.character_downward_speed = 0
+        self.character_vpos = self.screen_height / 2
+
+    @property
+    def is_game_over(self):
+        did_character_fall = self.character_vpos > self.screen_height
+        return did_character_fall 
+
+    @property
+    def screen_size(self):
+        return (self.screen_width, self.screen_height)
